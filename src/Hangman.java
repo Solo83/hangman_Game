69a72f1ac614e60e8277eac106149d83a83773e7
Solo83 <gstsamara@gmail.com>
2024-02-08 11:57:06 +0100

    FileReader Updated

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,28 +1,37 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class Hangman {
+    private static String getRandomLineFromFile() {
+        File f = new File("src/resources/dictionary.txt");
+        String str = null;
+        while (str == null) {
 
-    public static String randomWordFromFile(String path) throws IOException {
-
-        BufferedReader reader = new BufferedReader(new FileReader(path));
-        String line = reader.readLine();
-        List<String> wordsFromFile = new ArrayList<>();
-
-        while (line != null) {
-            String[] wordsFromLine = line.split("\\s+");
-            wordsFromFile.addAll(Arrays.asList(wordsFromLine));
-            line = reader.readLine();
+            try (RandomAccessFile rcf = new RandomAccessFile(f, "r");) {
+                long rand = (long) (new Random().nextDouble() * f.length());
+                rcf.seek(rand);
+                rcf.readLine();
+                str = new String(rcf.readLine().getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                System.out.println("Something went wrong.");
+            }
         }
 
-        Random r = new Random();
-
-        return wordsFromFile.get(r.nextInt(wordsFromFile.size()));
+        return str;
     }
 
-    public static String hangmanImage(int errorCounter) {
+    private static String getRandomWordFromLine() {
+        String line = getRandomLineFromFile();
+        String[] wordsFromLine = line.split("\\s+");
+        return Arrays.asList(wordsFromLine).get(new Random().nextInt(wordsFromLine.length));
+    }
+
+    private static boolean russianCharsetChecker(char letter) {
+         return Character.UnicodeBlock.of(letter).equals(Character.UnicodeBlock.CYRILLIC);
+    }
+
+    private static String hangmanImage(int errorCounter) {
         String s = "";
         switch (errorCounter) {
             case 0 ->
@@ -107,9 +116,9 @@ public class Hangman {
         return s;
     }
 
-    public static void gameModule() throws IOException {
+    private static void gameLogic() {
 
-        String randomWord = randomWordFromFile("resources/dictionary.txt").toLowerCase();
+        String randomWord = getRandomWordFromLine().toLowerCase();
         StringBuilder hiddenWord = new StringBuilder("_".repeat(randomWord.length()).toLowerCase());
         List<Character> errorLetters = new ArrayList<>();
         List<Character> enteredLetters = new ArrayList<>();
@@ -135,7 +144,13 @@ public class Hangman {
 
             System.out.printf("%nEnter letter: ");
 
-            char letter = Character.toLowerCase(scanner.next().charAt(0));
+            char letter = Character.toLowerCase(scanner.next().charAt(0));;
+
+            while(!russianCharsetChecker(letter)) {
+                System.out.println("Enter only Russian Letters!");
+                letter = Character.toLowerCase(scanner.next().charAt(0));
+            }
+
 
             if (enteredLetters.contains(letter)) {
                 System.out.printf("%nThis Letter has been already entered, try again! ");
@@ -162,24 +177,27 @@ public class Hangman {
 
     }
 
-    public static void main(String[] args) throws IOException {
-
+    public static void playTheGame() {
         boolean game = true;
         System.out.println("Do you wanna play the Game? y/n ");
 
         while (game) {
-
             String choose = String.valueOf(new Scanner(System.in).next().charAt(0)).toLowerCase();
-
             switch (choose) {
                 case "n" ->
-                    game = false;
+                        game = false;
                 case "y" -> {
-                    gameModule();
+                    gameLogic();
                     System.out.println();
                     System.out.println("Do you wanna play again? y/n ");
                 }
             }
         }
+    }
+
+    public static void main(String[] args) {
+
+        playTheGame();
+
     }
 }
